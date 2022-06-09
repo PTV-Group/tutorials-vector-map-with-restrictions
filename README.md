3071fad1a07adbc5429ff0295c89aa0964694cde
MachineUserPTV <github@ptvgroup.com>
2022-06-09 10:35:12 +0000

    Automatic synchronisation from developer.myptv.com

---
+++ README.md
@@ -0,0 +1,14 @@
+# Vector Map with Restrictions
+Display a map and visualize clickable truck restrictions like weight limits or truck-bans in inner cities.</br>
+</br>
+PTV Developer APIs used in this tutorial:
+- <a href="https://developer.myptv.com/Documentation/Vector%20Maps%20API/QuickStart.htm" target="_blank">Vector Maps API</a>
+- <a href="https://developer.myptv.com/Documentation/Raster%20Maps%20API/QuickStart.htm" target="_blank">Raster Maps API</a>
+
+## Getting started
+- Replayce the API key placeholder *"YOUR_API_KEY"* in `Rendering_Restrictions.js` with your <a href="https://myptv.com/developer" target="_blank">PTV Developer API key</a>.
+  - <a href="https://developer.myptv.com/Tutorials.htm" target="_blank">How to create an API key for PTV Developer</a>
+- Start the application by opening `index.html` in your browser.
+#
+### Try the tutorial application <a href="https://developer.myptv.com/Applications/MapRendering/Vector-Map-with-Restrictions/index.html" target="_blank">here</a>.
+### Read the <a href="https://developer.myptv.com/Tutorials/Map%20Rendering/Vector%20Map%20with%20Restrictions/Vector%20Map%20with%20Restrictions.htm" target="_blank">complete tutorial</a> on PTV Developer.
---
+++ Rendering_Restrictions.js
@@ -0,0 +1,55 @@
+const api_key = "YOUR_API_KEY";
+
+var coordinate = L.latLng(49.012, 8.4044); 
+var map = L.map('map', {
+  fullscreenControl: true
+}).setView(coordinate, 17);
+
+var vectorStyleUrl = "https://vectormaps-resources.myptv.com/styles/latest/standard.json";
+
+//Lazy load the plugin to support right-to-left languages such as Arabic and Hebrew.
+maplibregl.setRTLTextPlugin(
+  'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
+  null,
+  true 
+);
+
+var vectorLayer = L.maplibreGL({
+  attribution: '&copy; ' + new Date().getFullYear() + ' PTV Group, HERE',
+  interactive:false,
+  maxZoom: 18,
+  style: vectorStyleUrl,
+  transformRequest: (url) => {
+    let transformedUrl = url;
+    let mapsPathIndex = url.indexOf('/maps/');
+
+    if (mapsPathIndex > 0) {
+      transformedUrl = 'https://api.myptv.com/' + url.substring(mapsPathIndex) + '?apiKey=' + api_key;
+      return {
+        url: `${transformedUrl}`
+      };
+    } 
+    return null;
+  }
+}).addTo(map);
+
+map.createPane('clickableTiles');
+map.getPane('clickableTiles').style.zIndex = 500;
+var rasterTileUrl = "https://api.myptv.com/rastermaps/v1/data-tiles/{z}/{x}/{y}";
+var restrictionsLayer = L.tileLayer.ptvDeveloper(
+    rasterTileUrl + '?layers={layers}&apiKey=' + api_key, {
+    layers: 'restrictions',
+    maxZoom: 18,
+    opacity: 0.5,
+    pane: 'clickableTiles'
+  }).addTo(map);
+
+var layers = {
+    "Vector Base Map":  vectorLayer,
+    "Restrictions": restrictionsLayer,
+  
+};
+L.control.layers({}, layers, {
+    position: 'bottomleft',
+    autoZIndex: false
+}).addTo(map);
